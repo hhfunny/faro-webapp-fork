21234076acf0a8f0cb72f665951daa060228703f
hahafunny <hahafunny2222@protonmail.com>
2024-04-18 09:05:50 +0200

    Slider upgrade

--- src/main-components/Slider.jsx
+++ src/main-components/Slider.jsx
@@ -23,16 +23,16 @@ const Slider = () => {
 
   const [FadeImages] = useState([
     {
-      url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-      caption: 'Nazwa firmy'
+      title: 'Supporter name 1',
+      description: 'Supporter description 1'
     },
     {
-      url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
-      caption: 'Nazwa firmy'
+      title: 'Supporter name 2',
+      description: 'Supporter description 2'
     },
     {
-      url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-      caption: 'Nazwa firmy'
+      title: 'Supporter name 3',
+      description: 'Supporter description 3'
     },
     ]
   );
@@ -46,12 +46,9 @@ const Slider = () => {
           autoplay={false}
         >
           {FadeImages.map((fadeImage, index) => (
-            <div key={index}>
-              <img
-                style={{ width: '100%'}}
-                src={fadeImage.url}
-              />
-              <h2>{fadeImage.caption}</h2>
+            <div className='fade--item' key={index}>
+              <h2>{fadeImage.title}</h2>
+              <p>{fadeImage.description}</p>
             </div>
           ))}
         </Fade>
